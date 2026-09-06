9eab9e2c538c61c3e5777a952a81ecf9b5511be1
maksbruise7 <sinelnik0709@icloud.com>
2026-09-06 14:09:06 +0300

    Initial Commit

---
+++ MySwiftGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftGuide
+//
+//  Created by Максим on 06.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySwiftGuide/MySwiftGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftGuideApp.swift
+//  MySwiftGuide
+//
+//  Created by Максим on 06.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
